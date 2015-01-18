2c4dc68f5c98594bfd099e78a8afd351a03e6b78
cgspicer <cgspicer@gmail.com>
2015-01-18 18:17:52 -0500

    add: sub-menu reveal functionality

--- wp/wp-content/themes/aaronmarino/js/script.js
+++ wp/wp-content/themes/aaronmarino/js/script.js
@@ -1,6 +1,38 @@
 $(document).ready( function(){
   // aaronmarino.com
   // @author Coran Spicer
-  console.log( 'successfully loaded' );
+
+  // initialize the dropdown menus
+  if ( $('.menu-item-has-children').length > 0 ) {
+    $('.menu-item-has-children').each( function(){
+      var $submenu = $(this).find('ul.sub-menu');
+      var link = $(this).children('a')[0];
+      TweenMax.set( $submenu, { 'height': 0, 'opacity' : 0 } );
+      $submenu.on( 'mouseover', function(e){
+        console.log( 'wtf' );
+        $submenu.addClass('active');
+        $(link).unbind('mouseleave');
+        $submenu.one( 'mouseleave', function(e){
+          $submenu.removeClass('active');
+          TweenMax.to( $submenu, 1, { 'height': 'auto', 'opacity' : 0, 'onComplete' : function(){
+            TweenMax.set( $submenu, { 'display' : 'none' });
+          } } );
+        });
+      });
+      $(link).on( 'mouseover', function(e){
+        TweenMax.set( $submenu, { 'display' : 'block' });
+        TweenMax.to( $submenu, 1, {  'height': 'auto', 'opacity' : 1 } );
+        $(link).one('mouseleave', function(e){
+          setTimeout( function() {
+            if ( !$submenu.hasClass('active') ) {
+              TweenMax.to( $submenu, 1, { 'height': 'auto', 'opacity' : 0, 'onComplete' : function(){
+                TweenMax.set( $submenu, { 'display' : 'none' });
+              } });
+            }
+          }, 500 );
+        });
+      });
+    });
+  }
 
 });
