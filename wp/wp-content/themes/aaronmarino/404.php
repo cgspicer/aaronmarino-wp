a7a2727215de30085552085257f67a8f4748fd68
cgspicer <cgspicer@gmail.com>
2015-01-25 01:40:59 -0500

    add: 404 page

---
+++ wp/wp-content/themes/aaronmarino/404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Aaron Marino v2
+ */
+ get_header(); ?>
+<div class="center-wrapper">
+  <div class="grid-row">
+    <div class="col small-col-12 box gray inverted rounded-corners" style="margin-top: 2rem;">
+      <h2 style="text-align: center; font-size: 4rem; line-height: 4.5rem;"> 404 Not Found</h2>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
